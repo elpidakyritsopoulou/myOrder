7d2d8b1134168d9d7c73a45ddd9f3e2f706dc39d
elpidakyritsopoulou <62940074+elpidakyritsopoulou@users.noreply.github.com>
2021-01-04 21:43:23 +0200

    log in

--- Application/app/src/main/java/com/example/myorder/Order.java
+++ Application/app/src/main/java/com/example/myorder/Order.java
@@ -3,12 +3,13 @@ package com.example.myorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Order extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order);
-    }
+        setContentView(R.layout.order);
+}
+
 }
