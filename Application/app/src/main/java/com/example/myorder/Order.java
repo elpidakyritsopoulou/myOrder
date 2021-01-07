0054d434e8597c7b9178589e8c0300c90922e252
elpidakyritsopoulou <elpidakyritsopoulou@github.com>
2021-01-07 21:49:16 +0200

    Revert "update"
    This reverts commit c54ef64650a2da0fe0cb51c80175d0129b37942b.

--- Application/app/src/main/java/com/example/myorder/Order.java
+++ Application/app/src/main/java/com/example/myorder/Order.java
@@ -3,6 +3,7 @@ package com.example.myorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Order extends AppCompatActivity {
     @Override
