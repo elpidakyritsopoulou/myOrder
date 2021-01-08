ba724ab110006e834e12009f03a952a04b57bfb8
elpidakyritsopoulou <elpidakyritsopoulou@github.com>
2021-01-08 02:19:34 +0200

    changes

--- Application/app/src/main/java/com/example/myorder/Staff.java
+++ Application/app/src/main/java/com/example/myorder/Staff.java
@@ -1,16 +1,14 @@
 package com.example.myorder;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 public class Staff extends AppCompatActivity {
 
-   Button Staff1;
+    Button Staff1;
     Button Staff2;
     Button Staff3;
     Button Staff4;
@@ -21,8 +19,11 @@ public class Staff extends AppCompatActivity {
         setContentView(R.layout.activity_staff);
 
         Staff1 = findViewById(R.id.Staff1);
+        Staff2 = findViewById(R.id.Staff2);
+        Staff3 = findViewById(R.id.Staff3);
+        Staff4 = findViewById(R.id.Staff4);
 
-        Staff1.setOnClickListener(new  View.OnClickListener() {
+        Staff1.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -31,41 +32,6 @@ public class Staff extends AppCompatActivity {
 
             }
         });
-//
-//        Staff2 = findViewById(R.id.Staff2);
-//
-//       Staff2.setOnClickListener(new  View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent_one = new Intent(Staff.this, tables.class);
-//                startActivity(intent_one);
-//
-//            }
-//        });
-//
-//        Staff3 = findViewById(R.id.Staff3);
-//
-//        Staff3.setOnClickListener(new  View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent_one = new Intent(Staff.this, tables.class);
-//                startActivity(intent_one);
-//
-//            }
-//        });
-//
-//        Staff4 = findViewById(R.id.Staff4);
-//
-//        Staff4.setOnClickListener(new  View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent_one = new Intent(Staff.this, tables.class);
-//                startActivity(intent_one);
-//
-//            }
-//        });
+
     }
 }
