3f804d3a94d2d5b6e1dcc94e17d88921e84f5b54
Petrosdia <75843593+Petrosdia@users.noreply.github.com>
2021-01-21 00:58:17 +0200

    All

---
+++ Application/app/src/main/java/com/example/myorder/anapsiktika.java
@@ -0,0 +1,14 @@
+package com.example.myorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class anapsiktika extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anapsiktika);
+    }
+}
