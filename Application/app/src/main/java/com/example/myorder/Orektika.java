9bbc01919de60f7700cc9b89708e52ce91c28395
Petrosdia <75843593+Petrosdia@users.noreply.github.com>
2021-01-21 00:27:40 +0200

    Orektika

---
+++ Application/app/src/main/java/com/example/myorder/Orektika.java
@@ -0,0 +1,14 @@
+package com.example.myorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Orektika extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orektika);
+    }
+}
