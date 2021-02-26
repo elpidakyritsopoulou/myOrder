179af97e5bb7d102a6e8799dbefec5964ac6d68c
Petrosdia <75843593+Petrosdia@users.noreply.github.com>
2021-02-26 02:06:45 +0200

    PopUp

--- Application/app/src/main/java/com/example/myorder/PopActivity.java
+++ Application/app/src/main/java/com/example/myorder/PopActivity.java
@@ -6,14 +6,26 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class PopActivity extends Activity {
+
+    Button btn_close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop);
+
+        btn_close = (Button) findViewById(R.id.btn_close);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               finish();
+            }
+        });
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -21,7 +33,7 @@ public class PopActivity extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.8),  (int)(height*7));
+        getWindow().setLayout((int)(width*.8),(int)(height*.3));
 
         WindowManager.LayoutParams params= getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
