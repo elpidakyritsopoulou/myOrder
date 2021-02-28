b49445bc84c890c5afbc060d4aef2f7e93e84f32
Petrosdia <75843593+Petrosdia@users.noreply.github.com>
2021-02-28 22:40:09 +0200

    PopUp

--- Application/app/src/main/java/com/example/myorder/Menu.java
+++ Application/app/src/main/java/com/example/myorder/Menu.java
@@ -3,6 +3,7 @@ package com.example.myorder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -78,6 +79,7 @@ public class Menu extends AppCompatActivity implements FoodAdapter.OnFoodListene
 
 
     }
+
 
     @Override
     public void onFoodClick(int position) {
